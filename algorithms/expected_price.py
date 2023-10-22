a4b5ee0226226d2f50ff70a3f6e12077480a63ff
dutchie169 <dutchie169@gmail.com>
2023-10-22 18:06:23 +0200

    Implemented expect_price algo

--- algorithms/expected_price.py
+++ algorithms/expected_price.py
@@ -1,7 +1,36 @@
-from algorithms import IAlgorithm
+from algorithms import IAlgorithm, EffectiveTicketPrice
 from models import Instance, Schedule
+import copy
 
 class ExpectedPrice(IAlgorithm):
     def run(self, instance: Instance) -> Schedule:
-        return Schedule(instance)
 
+        expectedDays: List[instance.m] = copy.deepcopy(instance.days)
+        schedule: Schedule = Schedule(instance)
+
+        for i in range(instance.m):
+            #start with the first day as "average"
+            expectedDays[i].p_i = expectedDays[0].p_i
+            expectedDays[i].h_i = expectedDays[0].h_i
+            expectedDays[i].s_i = expectedDays[0].s_i
+            
+        for i in range(instance.m):
+            expectedDays = self._update_avg(expectedDays, instance, i)
+
+            s = EffectiveTicketPrice().run(Instance(instance.n, instance.m, expectedDays))
+            schedule.decisions.append(s.decisions[i])
+
+        return schedule
+
+    def _update_avg(self, exp_days, inst, d):
+        cur_avg = exp_days[d-1]
+
+        new_avg_p_i = (cur_avg.p_i*d + inst.days[d].p_i) / (d+1)
+        new_avg_h_i = (cur_avg.h_i*d + inst.days[d].h_i) / (d+1)
+        new_avg_s_i = (cur_avg.s_i*d + inst.days[d].s_i) / (d+1)
+
+        for i, day in enumerate(inst.days[d:], start = d):
+            exp_days[i].p_i = round(new_avg_p_i)
+            exp_days[i].h_i = round(new_avg_h_i)
+            exp_days[i].s_i = round(new_avg_s_i)
+        return exp_days
